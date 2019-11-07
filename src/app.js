c93c496436095fb8ff296e75a44d2d423246cacc
Luis Acevedo <acevedla@bc.edu>
2019-11-07 18:29:24 -0500

    added authentication with okta

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const { ExpressOIDC } = require('@okta/oidc-middleware')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
@@ -11,13 +15,31 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common'; 
 
+app.use(session({
+    secret: process.env.RANDOM_SECRET_WORD,
+    resave: true,
+    saveUninitialized: false
+}))
+
+const oidc = new ExpressOIDC({
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: process.env.REDIRECT_URL,
+    scope: 'openid profile',
+    routes: {
+        callback: {
+            path: '/authorization-code/callback',
+            defaultRedirect: '/admin'
+        }
+    }
+})
+
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
-
-app.get('/', (req, res) => {
-       res.send('Hello, world!')
-});
+app.use(oidc.router)
+app.use(bodyParser.json())
 
 app.use(function errorHandler(error, req, res, next) {
     let response
@@ -29,5 +51,27 @@ app.use(function errorHandler(error, req, res, next) {
     }
     res.status(500).json(response)
 })
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+
+app.get('/home', (req, res) => {
+    res.send('<h1>Welcome!!</div><a href="/login">Login</a>');
+})
+   
+app.get('/admin', oidc.ensureAuthenticated(), (req, res) =>{
+    res.send('Admin page');
+})
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/home');
+});
+  
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
 
 module.exports = app
